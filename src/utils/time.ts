fdd936c5ec20843787d167e28ac125a12c59c8e5
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2024-01-24 08:51:35 +1100

    (utils/time) improve date rendering

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -4,10 +4,17 @@ export const relative = (ms: number) => {
     const now = Date.now();
     const diff = now - ms;
 
+    if (diff > 0) return "now";
+
     const seconds = Math.abs(Math.floor((diff / 1000) % 60));
     const minutes = Math.abs(Math.floor((diff / (1000 * 60)) % 60));
     const hours = Math.abs(Math.floor((diff / (1000 * 60 * 60)) % 24));
     const days = Math.abs(Math.floor(diff / (1000 * 60 * 60 * 24)));
+
+    if (days < 1 && hours < 1 && minutes < 1 && seconds < 1) return "now";
+    if (days < 1 && hours < 1 && minutes < 1) return `${seconds}s`;
+    if (days < 1 && hours < 1) return `${minutes}:${seconds}`;
+    if (days < 1) return `${hours}:${minutes}:${seconds}`;
 
     return `${days}:${hours}:${minutes}:${seconds}`;
 };
