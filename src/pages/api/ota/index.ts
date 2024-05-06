95c4f6623e97bf521a853c00b721d7edeb7d2917
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2024-05-06 19:24:01 +1000

    (pages/api/ota) add ota api

---
+++ src/pages/api/ota/index.ts
@@ -0,0 +1,53 @@
+import type { APIRoute } from "astro";
+import type { JSX } from "solid-js/jsx-runtime";
+
+type NotificationProps = {
+    icon?: string;
+    iconUrl?: string;
+    title: string;
+    description: string;
+    level: "info" | "warning" | "error" | "success";
+    actions?: ({
+        children: JSX.Element | JSX.Element[];
+        type: "default" | "brand" | "danger" | "outline" | "positive";
+        href?: string;
+        className?: string;
+        disabled?: boolean;
+        dedupe?: boolean;
+        label: string;
+        rest?: JSX.ButtonHTMLAttributes<HTMLButtonElement>;
+    } & {
+        dismiss?: boolean;
+    })[];
+    timeout?: number;
+};
+
+export const GET: APIRoute = () => {
+    const notification: Partial<NotificationProps> = {
+        title: "Survey",
+        description:
+            "Support the project by completing a short survey about how you use RelaGit.",
+        level: "info",
+        actions: [
+            {
+                href: "",
+                label: "Complete Survey",
+                children: "Complete Survey",
+                dismiss: true,
+                type: "brand",
+            },
+            {
+                label: "No Thanks",
+                children: "No Thanks",
+                dismiss: true,
+                type: "default",
+            },
+        ],
+    };
+
+    return new Response(JSON.stringify(notification), {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+};
